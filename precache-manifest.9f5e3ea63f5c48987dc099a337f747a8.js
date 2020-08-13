64d9ba57def519f35fa204f8e243264ab300ccfd
Arjun Mohandas <48945581+Arjunmdas3211@users.noreply.github.com>
2020-08-13 16:36:35 +0530

    Updates

---
+++ precache-manifest.9f5e3ea63f5c48987dc099a337f747a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "154055d64c72802ce509b1274c1c0c9c",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "bc2bcde21f47ce43d518",
+    "url": "/monster/static/css/main.011be275.chunk.css"
+  },
+  {
+    "revision": "42d9d5d8dfa5a3681d85",
+    "url": "/monster/static/js/2.9dd08065.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster/static/js/2.9dd08065.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc2bcde21f47ce43d518",
+    "url": "/monster/static/js/main.c892c658.chunk.js"
+  },
+  {
+    "revision": "77f0cac0c6ee332825ff",
+    "url": "/monster/static/js/runtime-main.78ba007a.js"
+  }
+]);
